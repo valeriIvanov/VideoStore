51c13465e1e287ced07d886a18fbfcd337385340
valeriIvanov <valeri.ivanov.pz@gmail.com>
2016-05-19 00:36:45 +0300

    Signed-off-by: valeriIvanov <valeri.ivanov.pz@gmail.com>

--- VideoStore.Repository/Properties/AssemblyInfo.cs
+++ VideoStore.Repository/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VideoClub.Repository")]
+[assembly: AssemblyTitle("VideoStore.Repository")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
